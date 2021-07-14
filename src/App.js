c9379c1ca5d8a10e86fb7bbfab42323dc6f417fb
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-14 17:20:38 +0200

    add base structure, dividing 2 components (header and body)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Body />
     </div>
   );
 }
 
-export default App;
+// creating the header
+
+function Header() {
+  return (
+    <form className="headerForm">
+      <input
+        className="headerInput"
+        type="text"
+        placeholder="type in your grocery"
+      ></input>
+      <button className="headerButton" type="submit">
+        Add a Grocery
+      </button>
+    </form>
+  );
+}
+
+//creating the body
+
+function Body() {
+  return (
+    <ul>
+      <li>
+        Apple
+        <p>X</p>
+        <button>Complete</button>
+      </li>
+      <li>
+        Shampoo
+        <p>X</p>
+        <button>Pending</button>
+      </li>
+    </ul>
+  );
+}
