9eecdfb57124f9f2f53eef8884faa7606b9520dc
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-14 22:38:40 +0200

    add rvalue rendering functionality onsubmit event

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Your Shopping List</h1>
-      <Header />
-      <Body />
+      <Main />
     </div>
   );
 }
 
-// creating the header
-function handleAddGrocerySubmit(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formInput = form.shoppingForm;
-  const inputValue = formInput.value;
-  console.log(inputValue);
-}
+// main app component
 
-function Header() {
+function Main({ name }) {
+  const [state, setState] = useState([]);
+
+  //event onsbumit and get the value of the form.
+  function handleAddGrocerySubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formInput = form.shoppingForm;
+    const inputValue = formInput.value;
+
+    //concatenate through spread.. the state (which is empty) and the inputvalue - as an array.
+    const newProductItems = [...state, inputValue];
+
+    //set to new state the productitem.
+    setState(newProductItems);
+    form.reset();
+    console.log(newProductItems);
+  }
+
+  function renderProducts() {
+    //state is here the new state and we are getting the date (single product), through mapping. we return the component "Product" we created and pass it the prop name and the vale.
+    const listItems = state.map((singleProduct) => {
+      return <Product name={singleProduct} />;
+    });
+    return listItems;
+  }
+  let fakeh1 = "Your Shopping List";
   return (
-    <form onSubmit={handleAddGrocerySubmit} className="headerForm">
-      <input
-        className="headerInput"
-        name="shoppingForm"
-        id="shoppingForm"
-        type="text"
-        placeholder="type in your grocery"
-      ></input>
-      <button className="headerButton" type="submit">
-        Add a Grocery
-      </button>
-    </form>
+    <div>
+      <h1>{fakeh1}</h1>
+      <form onSubmit={handleAddGrocerySubmit} className="headerForm">
+        <input
+          className="headerInput"
+          name="shoppingForm"
+          id="shoppingForm"
+          type="text"
+          required
+          placeholder="type in your grocery"
+        ></input>
+        <button className="headerButton" type="submit">
+          Add a Grocery
+        </button>
+      </form>
+      <ul>{renderProducts()}</ul>
+    </div>
   );
 }
 
-//creating the body
-
-function Body() {
-  return (
-    <ul>
-      <li>
-        Apple
-        <p>X</p>
-        <button>Complete</button>
-      </li>
-      <li>
-        Shampoo
-        <p>X</p>
-        <button>Pending</button>
-      </li>
-    </ul>
-  );
+//product component
+function Product({ name }) {
+  return <li>{name}</li>;
 }
