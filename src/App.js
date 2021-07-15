af56eb3aa681796868d2658b8901a239d035dede
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-15 15:43:18 +0200

    finished functionalities for shopping list

--- src/App.js
+++ src/App.js
@@ -15,40 +15,69 @@ export default function App() {
 function Main({ name }) {
   const [state, setState] = useState([]);
 
+  //here we say: clickedproduct (because later, handleclickproduct is beeing passed into the onclick props), is not equal to
+  function handleClickProduct(clickedProduct) {
+    const newProducts = state.filter((gefilterteProdukte) => {
+      return gefilterteProdukte.name !== clickedProduct;
+    });
+    setState(newProducts);
+  }
+
   //event onsbumit and get the value of the form.
   function handleAddGrocerySubmit(event) {
     event.preventDefault();
+    //that event target works because, handlegrocerysubmit function is beeing called in the form.
     const form = event.target;
     const formInput = form.shoppingForm;
     const inputValue = formInput.value;
 
-    //concatenate through spread.. the state (which is empty) and the inputvalue - as an array.
-    const newProductItems = [...state, inputValue];
+    //concatenate through spread.. the state (which is empty at first, and then then gets the previous status) and the inputvalue - as an array.
+    const newProductItems = [
+      ...state,
+      {
+        name: inputValue,
+        isBought: false,
+      },
+    ];
 
     //set to new state the productitem.
     setState(newProductItems);
     form.reset();
-    console.log(newProductItems);
   }
 
-  //here we say: clickedproduct (because later, handleclickproduct is beeing passed into the onclick props), is not equal to
-  function handleClickProduct(clickedProduct) {
-    const newProducts = state.filter(
-      (gefilterteProdukte) => gefilterteProdukte !== clickedProduct
-    );
-    setState(newProducts);
+  function handleClickIsBought(productName) {
+    const newProductsIsBought = state.map((productObject) => {
+      if (productObject.name === productName) {
+        return {
+          ...productObject,
+          isBought: !productObject.isBought,
+        };
+      }
+      return productObject;
+    });
+
+    setState(newProductsIsBought);
+    console.log(newProductsIsBought);
   }
+
   function renderProducts() {
     //state is here the new state and we are getting the date (single product), through mapping. we return the component "Product" we created and pass it the prop name and the vale.
-    const listItems = state.map((singleProduct) => {
-      return <Product name={singleProduct} onClick={handleClickProduct} />;
+    const listItems = state.map((singleProduct, index) => {
+      return (
+        <Product
+          key={index}
+          product={singleProduct}
+          onClickItem={handleClickProduct}
+          onClickIsBought={handleClickIsBought}
+        />
+      );
     });
     return listItems;
   }
-  let fakeh1 = "Your Shopping List";
+  let headerTitleH1 = "Your Shopping List";
   return (
     <div>
-      <h1>{fakeh1}</h1>
+      <h1>{headerTitleH1}</h1>
       <form onSubmit={handleAddGrocerySubmit} className="headerForm">
         <input
           className="headerInput"
--- src/product.js
+++ src/product.js
@@ -1,7 +1,29 @@
 //product component
-export default function Product({ name, onClick }) {
-  function handleClick() {
-    onClick(name);
+export default function Product({ product, onClickItem, onClickIsBought }) {
+  function handleButtonClick(event) {
+    event.stopPropagation();
+    onClickIsBought(product.name);
   }
-  return <li onClick={handleClick}>{name}</li>;
+
+  function handleClick() {
+    onClickItem(product.name);
+  }
+
+  let boughtText;
+  let shoppingListItemClass;
+
+  if (product.isBought) {
+    shoppingListItemClass = "listClass--itemRed";
+    boughtText = "Bought";
+  } else {
+    boughtText = "Buy";
+  }
+
+  return (
+    <li className={`listClass ${shoppingListItemClass}`}>
+      <p onClick={handleClick}>X</p>
+      {product.name}
+      <button onClick={handleButtonClick}>{boughtText}</button>
+    </li>
+  );
 }
