c2543e3a5f31d5c53cd4b48c726f9012171f6183
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-16 16:10:40 +0200

    add render functionality + button

--- src/App.js
+++ src/App.js
@@ -10,26 +10,33 @@ export default function App() {
 }
 
 function Main() {
-  const [state, setState] = useState([""]);
+  const [state, setState] = useState([]);
   function productObject(event) {
     event.preventDefault();
     const form = event.target;
     const formInput = form.input;
     const formInputValue = formInput.value;
-    const addedProduct = [...state, formInputValue];
+    const addedProduct = [...state, { Name: formInputValue, gekauft: false }];
     setState(addedProduct);
     form.reset();
   }
 
-  const blabliblu = state.map((newItem) => {
-    return <li>{newItem}</li>;
+  const blablibluName = state.map((newItem) => {
+    return (
+      <>
+        <button>Whatsapp?</button>
+        <li>{[newItem.Name]}</li>
+      </>
+    );
   });
+  console.log(blablibluName);
 
   return (
     <>
       <h1>Shopping List</h1>
       <form onSubmit={productObject}>
         <input
+          required
           name="input"
           id="input"
           type="text"
@@ -37,7 +44,7 @@ function Main() {
         ></input>
         <button type="submit">Add an Item</button>
       </form>
-      <ul>{blabliblu}</ul>
+      <ul>{blablibluName}</ul>
     </>
   );
 }
