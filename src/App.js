10ced2c112a09679f24e79df7c62637485562199
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-18 23:11:04 +0200

    created following features: render items, remove onbuttonclick, and change true/false status (didnt work well, since there is an error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
@@ -8,6 +9,97 @@ export default function App() {
   );
 }
 
-// main app component
+function Product({ productItem, itemClick, itemChangeStatus }) {
+  function handleOnClick() {
+    itemClick(productItem.name);
+  }
 
-function Main() {}
+  function changeStatusButton() {
+    itemChangeStatus(productItem.name);
+  }
+
+  return (
+    <li className="list-class">
+      {productItem.name}
+      <button onClick={handleOnClick}>X</button>
+      <button onClick={changeStatusButton}>Buy</button>
+    </li>
+  );
+}
+
+// main app component
+function Main() {
+  const [state, setState] = useState([]);
+
+  function submitDiv(Event) {
+    Event.preventDefault();
+    const form = Event.target;
+    const input = form.formInput;
+    const inputValue = input.value;
+    const productObject = [
+      ...state,
+      {
+        status: false,
+        name: inputValue,
+      },
+    ];
+    setState(productObject);
+    form.reset();
+    console.log(productObject);
+  }
+
+  function changeStatus(clickedItemButton) {
+    const changeItem = state.map((clickedItem) => {
+      if (clickedItem.name === clickedItemButton) {
+        return {
+          ...clickedItem,
+          status: !clickedItem.status,
+        };
+      }
+      return changeItem;
+    });
+    setState(changeItem);
+    console.log(changeItem);
+  }
+
+  function Div({ h1Title, submitButtonText }) {
+    return (
+      <div onSubmit={submitDiv}>
+        <h1>To do App from {h1Title}</h1>
+        <form>
+          <input
+            id="formInput"
+            name="formInput"
+            type="text"
+            placeholder="your desired product"
+            required
+          />
+          <button type="submit">{submitButtonText}</button>
+        </form>
+        <ul>{renderProduct()}</ul>
+      </div>
+    );
+  }
+
+  function renderProduct() {
+    const renderedObject = state.map((productItem) => {
+      return (
+        <Product
+          productItem={productItem}
+          itemClick={removeItem}
+          itemChangeStatus={changeStatus}
+        />
+      );
+    });
+    return renderedObject;
+  }
+
+  function removeItem(clickedProduct) {
+    const removeItem = state.filter(
+      (clickedItem) => clickedItem.name !== clickedProduct
+    );
+    setState(removeItem);
+    return removeItem;
+  }
+  return <Div h1Title="Mike" submitButtonText="Click me" />;
+}
